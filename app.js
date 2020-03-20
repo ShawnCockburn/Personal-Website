942261247482c41f3ddf37c2ee74454effe813a7
ShawnCockburn <shawncockburn@hotmail.com>
2020-03-20 18:52:24 +0000

    Projects now populate from mongoDB

--- app.js
+++ app.js
@@ -19,69 +19,44 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static('public'));
 
-//test
-
-let projects = [{
-  name: "Test Project",
-  caption: "This is a test project for testing purposes",
-  imgSrc: "https://picsum.photos/200/350"
-},
-{
-  name: "Test Project",
-  caption: "This is a test project for testing purposes",
-  imgSrc: "https://picsum.photos/200/350"
-},
-{
-  name: "Test Project",
-  caption: "This is a test project for testing purposes",
-  imgSrc: "https://picsum.photos/200/350"
-},
-{
-  name: "Test Project",
-  caption: "This is a test project for testing purposes",
-  imgSrc: "https://picsum.photos/200/350"
-},
-{
-  name: "Test Project",
-  caption: "This is a test project for testing purposes",
-  imgSrc: "https://picsum.photos/200/350"
-},
-{
-  name: "Test Project",
-  caption: "This is a test project for testing purposes",
-  imgSrc: "https://picsum.photos/200/350"
-},
-{
-  name: "Test Project",
-  caption: "This is a test project for testing purposes",
-  imgSrc: "https://picsum.photos/200/350"
-},
-{
-  name: "Test Project",
-  caption: "This is a test project for testing purposes",
-  imgSrc: "https://picsum.photos/200/350"
-},
-{
-  name: "Test Project",
-  caption: "This is a test project for testing purposes",
-  imgSrc: "https://picsum.photos/200/350"
-}
-];
 
 //Setup database
 
-mongoose.connect('mongodb://localhost:27017/content', {
+mongoose.connect('mongodb://localhost:27017/contentDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
 
 const Schema = mongoose.Schema;
 
+const projectSchema = new Schema({
+  name: String,
+  caption: String,
+  imgSrc: String
+});
+
+const Project = mongoose.model("project", projectSchema);
+
 //routes
 
 app.route("/")
   .get(function(req, res) {
-    res.render("index.ejs", {projects: projects});
+    let projects = [];
+
+    Project.find(function(err, results) {
+      if (!err) {
+        if (results || results != "") {
+          projects = results;
+        } else {
+          console.log("No projects");
+        }
+      } else {
+        console.log(err);
+      }
+      res.render("index.ejs", {
+        projects: projects
+      });
+    });
   });
 
 app.route("/blog")
@@ -90,6 +65,11 @@ app.route("/blog")
       posts: posts
     });
   });
+
+//404
+app.get('*', function(req, res) {
+  res.send('what???', 404);
+});
 
 //server start
 
