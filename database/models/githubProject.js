a479ea59be86054ab4b428fbb71ca09628d58243
ShawnCockburn <shawncockburn@hotmail.com>
2020-07-05 12:14:36 +0100

    Completly new design and backend

---
+++ database/models/githubProject.js
@@ -0,0 +1,13 @@
+// const Mongoose = require("../mongoose");
+
+// const Schema = Mongoose.Schema;
+
+// const githubProjectSchema = new Schema({
+//     githubProjectId: String,
+//     languagesApiUrl: String,
+//     imageUrl: String,
+// });
+
+// const CartItem = mongoose.model('CartItem', cartItemSchema);
+
+// module.exports = CartItem;
--- public/js/tiltFx.js
+++
@@ -1,7 +0,0 @@
-$(".project-box").tilt({
-scale: 1.1,
-speed:          500,
-transition:     true,
-maxTilt:        20,
-perspective:    1000
-});
