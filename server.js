002da82ba71033a128827f388afb0715d72b11a6
shawn <shawncockburn@hotmail.com>
2020-07-07 20:35:44 +0100

    fixed bug that allowed http connection without redirecting to https

--- server.js
+++ server.js
@@ -1,25 +1,14 @@
+const devTest = process.env.dev;
+
 // Dependencies
 const fs = require('fs');
 const http = require('http');
 const https = require('https');
 const express = require('express');
 const github = require("./util/githubProjects");
-
 const app = express();
 
-//app
-
-app.use("/assets", express.static('assets'));
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/views");
-
-app.get("/", (req, res) => {
-    github.projects().then(projects => res.render("index", { githubProjects: projects }));
-});
-
-const devTest = process.env.dev;
-
-if (!devTest) {
+const sslServer = () => {
     // Certificate
     const privateKey = fs.readFileSync('./SSL/private_key.key', 'utf8');
     const certificate = fs.readFileSync('./SSL/ssl_certificate.cer', 'utf8');
@@ -28,13 +17,7 @@ if (!devTest) {
         key: privateKey,
         cert: certificate
     };
-    //force ssl
-    app.use((req, res, next) => {
-        if (req.connection.encrypted) return next();
-        res.redirect(301, 'https://' + req.headers.host + req.url);
-    });
 
-    
     // Starting http server
     const httpServer = http.createServer(app);
     const httpport = 80;
@@ -48,13 +31,39 @@ if (!devTest) {
     httpsServer.listen(httpsport, () => {
         console.log('HTTPS Server running');
     });
-} else {
+};
+
+const testServer = () => {
     // Starting http test server
     const httpTestServer = http.createServer(app);
     const httpTestPort = 3000;
     httpTestServer.listen(httpTestPort, () => {
         console.log('HTTP test server running on port: ' + httpTestPort);
     });
+};
+
+//app
+
+app.use("/assets", express.static('assets'));
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+//force ssl if in preduction
+if (!devTest) app.use((req, res, next) => {
+    if (req.connection.encrypted) return next();
+    res.redirect(301, 'https://' + req.headers.host + req.url);
+});
+
+app.get("/", (req, res) => {
+    github.projects().then(projects => res.render("index", { githubProjects: projects }));
+});
+
+
+//check if started in dev mode
+if (!devTest) {
+    sslServer();
+} else {
+    testServer();
 }
 
 
