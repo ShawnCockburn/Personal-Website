e594adbbc44cc5a88468ccf720846848742824aa
shawn <shawncockburn@hotmail.com>
2020-07-07 16:33:58 +0100

    Added SSL

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
-const express = require("express");
-const app = express();
+// Dependencies
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
+const express = require('express');
 const github = require("./util/githubProjects");
-const port = process.env.PORT || 443;
+
+// Certificate
+const privateKey = fs.readFileSync('./SSL/private_key.key', 'utf8');
+const certificate = fs.readFileSync('./SSL/ssl_certificate.cer', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate
+};
+
+//app
 
 app.use("/assets", express.static('assets'));
 app.set("view engine", "ejs");
@@ -11,7 +24,16 @@ app.get("/", (req, res) => {
     github.projects().then(projects => res.render("index", {githubProjects: projects}));    
 });
 
-app.listen(port, () => {
-    console.log("server started");
+// app.listen(port, () => {
+//     console.log("server started");
     
+// });
+
+// Starting https server
+const httpsServer = https.createServer(credentials, app);
+const port = 443;
+httpsServer.listen(port, () => {
+	console.log('HTTPS Server running');
 });
+
+
