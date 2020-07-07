b3ffff2dde4ba5b4ac94d8299d7d5e3a1882888a
shawn <shawncockburn@hotmail.com>
2020-07-07 19:13:47 +0100

    Fixed a bug that prevented jquery from loading and added a test option

--- server.js
+++ server.js
@@ -7,50 +7,54 @@ const github = require("./util/githubProjects");
 
 const app = express();
 
-// Certificate
-const privateKey = fs.readFileSync('./SSL/private_key.key', 'utf8');
-const certificate = fs.readFileSync('./SSL/ssl_certificate.cer', 'utf8');
-
-const credentials = {
-	key: privateKey,
-	cert: certificate
-};
-
 //app
 
 app.use("/assets", express.static('assets'));
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
-//force ssl
-app.get('*',(req,res,next) => {
-    if(req.headers['x-forwarded-proto']!='https')
-      res.redirect('https://www.shawncockburn.co.uk'+req.url)
-    else
-      next();
-  })
-
 app.get("/", (req, res) => {
-    github.projects().then(projects => res.render("index", {githubProjects: projects}));    
+    github.projects().then(projects => res.render("index", { githubProjects: projects }));
 });
 
-// app.listen(port, () => {
-//     console.log("server started");
+const devTest = process.env.dev;
+
+if (!devTest) {
+    // Certificate
+    const privateKey = fs.readFileSync('./SSL/private_key.key', 'utf8');
+    const certificate = fs.readFileSync('./SSL/ssl_certificate.cer', 'utf8');
+
+    const credentials = {
+        key: privateKey,
+        cert: certificate
+    };
+    //force ssl
+    app.use((req, res, next) => {
+        if (req.connection.encrypted) return next();
+        res.redirect(301, 'https://' + req.headers.host + req.url);
+    });
+
     
-// });
+    // Starting http server
+    const httpServer = http.createServer(app);
+    const httpport = 80;
+    httpServer.listen(httpport, () => {
+        console.log('HTTP Server running');
+    });
 
-// Starting http server
-const httpServer = https.createServer(app);
-const httpport = 80;
-httpServer.listen(httpport, () => {
-	console.log('HTTP Server running');
-});
-
-// Starting https server
-const httpsServer = https.createServer(credentials, app);
-const httpsport = 443;
-httpsServer.listen(httpsport, () => {
-	console.log('HTTPS Server running');
-});
+    // Starting https server
+    const httpsServer = https.createServer(credentials, app);
+    const httpsport = 443;
+    httpsServer.listen(httpsport, () => {
+        console.log('HTTPS Server running');
+    });
+} else {
+    // Starting http test server
+    const httpTestServer = http.createServer(app);
+    const httpTestPort = 3000;
+    httpTestServer.listen(httpTestPort, () => {
+        console.log('HTTP test server running on port: ' + httpTestPort);
+    });
+}
 
 
