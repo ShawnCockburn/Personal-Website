d3beda000bca413616da110d6102ba1c9d2882eb
Shawn Cockburn <shawn@Administrators-iMac.local>
2020-07-21 13:44:33 +0100

    Fixed Mobile Nav Bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,7 +22,7 @@
 
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-toggle i').toggleClass('fa-bars fa-times');
           $('.mobile-nav-overly').fadeOut();
         }
         return false;
@@ -49,12 +49,12 @@
       class: 'mobile-nav d-lg-none'
     });
     $('body').append($mobile_nav);
-    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="icofont-navigation-menu"></i></button>');
+    $('body').prepend('<button type="button" class="mobile-nav-toggle d-lg-none"><i class="fa fa-bars"></i></button>');
     $('body').append('<div class="mobile-nav-overly"></div>');
 
     $(document).on('click', '.mobile-nav-toggle', function (e) {
       $('body').toggleClass('mobile-nav-active');
-      $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+      $('.mobile-nav-toggle i').toggleClass('fa-bars fa-times');
       $('.mobile-nav-overly').toggle();
     });
 
@@ -69,7 +69,7 @@
       if (!container.is(e.target) && container.has(e.target).length === 0) {
         if ($('body').hasClass('mobile-nav-active')) {
           $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('icofont-navigation-menu icofont-close');
+          $('.mobile-nav-toggle i').toggleClass('fa-bars fa-times');
           $('.mobile-nav-overly').fadeOut();
         }
       }
