1da37fcb049e502f9b2f80242498d3e0884778ea
Shawn Cockburn <shawn@Shawns-MacBook-Pro.local>
2020-09-18 19:00:34 +0100

    Changed image and added subtext

--- util/githubProjects.js
+++
@@ -1,9 +0,0 @@
-const fetch = require('node-fetch');
-
-const apiUrl = "https://api.github.com/users/ShawnCockburn/repos";
-const projects = async () => {
-    return await (await fetch(apiUrl)).json();
-};
-
-
-module.exports.projects = projects;
