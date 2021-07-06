151a526d5053a27057a4be7137f5d7c1a70180cf
ShawnCockburn <shawncockburn@hotmail.com>
2021-07-06 20:45:54 +0100

    add: docs and react component styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import H1 from './H1'
 
 const Home = () => {
-    return (<></>)
+    return (<>
+        <H1>test</H1>
+    </>)
 }
 
 export default Home
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./Home";
 
+/**
+ * @component
+ * @param {*} props 
+ * @returns 
+ */
+
 const Navigation = (props) => {
   return (
     <Router>
