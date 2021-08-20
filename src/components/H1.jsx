7f94c514a1b2380c919847bbdcd0a3aec51d4c67
ShawnCockburn <shawncockburn@hotmail.com>
2021-08-20 12:42:29 +0100

    push

--- src/components/H1.jsx
+++ src/components/H1.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import propTypes from "prop-types"
 
 /**
  * H1 Component
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import H1 from './H1'
+import Container from './Container'
 
 const Home = () => {
     return (<>
+    <Container>
         <H1>test</H1>
+    </Container>
     </>)
 }
 
